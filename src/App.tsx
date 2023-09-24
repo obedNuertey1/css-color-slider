2d8a4001085a668a0ef2dda79df0ad516cf90c12
Obed Nuertey <onuertey1997@gmail.com>
2023-09-24 17:03:34 +0000

    update: Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = ():JSX.Element => {
   return (
     <>
       <h1 className="text-center">CSS Color Markers</h1>
-      <div className="container">
+      <div className="container conte">
         <div className="marker ml-auto mr-auto mb-2.5 mt-2.5 w-52 h-6 bg-red-600"></div>
         <div className="marker ml-auto mr-auto mb-2.5 mt-2.5 w-52 h-6 bg-green-600"></div>
         <div className="marker ml-auto mr-auto mb-2.5 mt-2.5 w-52 h-6 bg-blue-600"></div>
